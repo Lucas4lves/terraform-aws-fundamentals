a124d88b6b74654c9da5ab57f8d7a79a3fd5ed04
lucas4lves <zombnfl@gmail.com>
2023-03-26 23:34:11 -0300

    Bash script added

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
     required_providers{
 		aws = {
-				source = "hashicorp/aws"
-				version = "~> 4.0"
+		source = "hashicorp/aws"
+		version = "~> 4.0"
 		      }
 		}
 }
@@ -15,4 +15,14 @@ provider "aws" {
 resource "aws_instance" "app_server" {
 		ami = "ami-0735c191cf914754d"
 		instance_type = "t2.micro"
+		key_name="terraform"
+		user_data= <<EOF
+					 #!bin/bash
+					 cd /home/ubuntu
+					 echo "<h1>Alou Terraform</h1>"
+					 nohup busybox httpd -f -p 8080
+					 EOF
+		tags = {
+		    Name = "Teste de novo"
+		}
 }
